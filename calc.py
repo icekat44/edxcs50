2d36f4edfaf80eb1688ffdf1f4172d37dded397d
icekat44 <icekat44@gmail.com>
2025-05-31 02:10:45 -0400

    Commit results of learning about python int

---
+++ calc.py
@@ -0,0 +1,12 @@
+# Make a calculator
+# Declare variables
+# x = input("What's x? ")
+# y = input("What's y? ")
+x = int(input("WHat's x? "))
+y = int(input("What's y? "))
+
+# z = int(x) + int(y)
+
+# Print output
+# print(z)
+print(x + y)
