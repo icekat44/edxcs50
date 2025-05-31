d3106a8735cbabf3c4ed967310c2a869d8cb82ea
icekat44 <icekat44@gmail.com>
2025-05-31 01:51:14 -0400

    Commit results of learning about python str

---
+++ Hello2.py
@@ -0,0 +1,14 @@
+# Ask User for their name then strip unnecessary whiteapse and capitalize
+name = input("What's your name? ").strip().title()
+
+# Remove any unnecessary whitespace from str
+# name = name.strip()
+
+# Capitalize user's name
+# name = name.capitalize()
+
+# Title also capitalizes
+# name = name.title()
+
+# Display 'Hello' usernsme to user
+print("Hello,", name)
