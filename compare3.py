c2b492175b47d9f5f4f41f80ef3344c3732eb80d
icekat44 <icekat44@gmail.com>
2025-06-03 00:00:16 -0400

    Commit results of Pyrhton using conditionals w/ or-and

---
+++ compare3.py
@@ -0,0 +1,16 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# if x < y:
+# if x < y or x > y:
+if x != y:
+    print("x is not equal to y")
+#    print("x is less than y")
+else:
+    print ("x is equal to y")
+
+# elif x > y:
+#   print("x is greater than y")
+# elif x == y:
+# else:
+#    print("x is equal to y")
